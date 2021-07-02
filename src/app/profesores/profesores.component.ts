883a9445358a6f8070923673c7c441bc6eec3efb
nacardozog <nacardozog@heon.com.co>
2021-07-02 18:04:21 -0500

    Ajusto el recibido de los profesores

--- src/app/profesores/profesores.component.ts
+++ src/app/profesores/profesores.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-profesores',
@@ -6,10 +8,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profesores.component.css']
 })
 export class ProfesoresComponent implements OnInit {
-
-  constructor() { }
+  listaProfesores: any = [];
+  constructor(private http: HttpClient,) { }
 
   ngOnInit(): void {
+    this.consumirAPIProfesoras()
+  }
+
+  consumirAPIProfesoras() {
+    this.http.get('http://hp-api.herokuapp.com/api/characters/staff' ).subscribe(
+      (data: Array<any>) => {
+        this.listaProfesores = data;
+        this.listaProfesores.forEach(element => {
+          if (element.dateOfBirth != '') {
+            element.Age = moment().diff(moment(element.dateOfBirth, "DD-MM-YYYY"), 'years');
+          }
+          else {
+            element.Age = ''
+          }
+        });
+      }, error => {
+        console.log(error);
+      });
   }
 
 }
